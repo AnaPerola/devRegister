18ed7b70283bf88c493dce3254c78a4908a7d86a
Ana Perola <anaperolinha@hotmail.com>
2022-03-27 16:58:47 -0300

    Created views and route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'user/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
